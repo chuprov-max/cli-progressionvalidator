b721cdb8ad7267576a5f05ffa4d9c0d944bded17
Maxim <chuprov.maxim@gmail.com>
2017-09-12 18:13:24 +0300

    check empty argument

--- script.php
+++ script.php
@@ -1,5 +1,9 @@
 <?php
 require_once ('src/ProgressionValidator.php');
 
-$validator = new src\ProgressionValidator($argv[1]);
-$validator->validate();
+if (isset($argv[1])) {
+   $validator = new src\ProgressionValidator($argv[1]);
+   $validator->validate();    
+} else {
+    echo 'You need to specify an argument';
+}
